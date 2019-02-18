ff75f50a842de17c82378cf594987042af57f3b5
WDante <logan.trecartin@gmail.com>
2019-02-18 13:20:44 -0500

    initial commit

---
+++ PluralsiteTestResult.cs
@@ -0,0 +1,15 @@
+namespace testing_console
+{
+    using System;
+    public class PluralsiteTestResult
+    {
+        public string id {get;set;}
+        public string title {get;set;}
+        public string url {get;set;}
+        public string thumbnailUrl {get;set;}
+        public int score {get;set;}
+        public double percentile {get;set;}
+        public string level {get;set;}
+        public DateTime dateCompleted {get;set;}
+    }
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Purpose
+## To pull the most up to date test results from your pluralsight profile and export the html formatted results so that you can drop them into an html file (with the attached CSS), and have it work. 
+
+# Getting Started
+- Clone Repo
+- Place your pluralsight id in the appsettings.json file
+- remove "_ex" from the file name
+- run project
+- copy output in badges.txt to the body of the test.html file
+- view html file
